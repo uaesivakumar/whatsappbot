7fa84b4758b0d902f377de972f00807077a16b26
Sivakumar Chandrasekaran <uaesivakumar@gmail.com>
2025-09-06 00:15:34 +0400

    fix(console): handle /console and /console/*; set Vite base

--- server.js
+++ server.js
@@ -127,6 +127,11 @@ const app = express();
 app.use(express.json({ limit: "2mb" }));
 
 app.use("/console", express.static(path.join(__dirname, "console/dist")));
+const serveConsole = (_, res) => res.sendFile(path.join(__dirname, "console/dist/index.html"));
+app.get("/console", serveConsole);
+app.get("/console/*", serveConsole);
+
+app.use("/console", express.static(path.join(__dirname, "console/dist")));
 app.get("/console/*", (_, res) => { res.sendFile(path.join(__dirname, "console/dist/index.html")); });
 
 app.use("/console", express.static(path.join(__dirname, "console/dist")));
